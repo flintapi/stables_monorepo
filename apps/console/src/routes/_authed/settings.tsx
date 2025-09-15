2728c48634ad96a8196aa0c5f00711e1a79a4ca8
Miracle Friday <miraclef60@gmail.com>
2025-09-15 12:57:33 +0100

    refactor: Modify settings page abstract tabs into separate functional components

--- apps/console/src/routes/_authed/settings.tsx
+++ apps/console/src/routes/_authed/settings.tsx
@@ -2,6 +2,7 @@ import { createFileRoute } from '@tanstack/react-router'
 import z from 'zod'
 import { ChevronsUpDown, Copy, PlusCircle, Trash2 } from 'lucide-react'
 import { IconGlobe } from '@tabler/icons-react'
+import type { FC } from 'react'
 import { Container, Main, Section } from '@/components/craft'
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import { Badge } from '@/components/ui/badge'
@@ -30,6 +31,39 @@ export const Route = createFileRoute(`/_authed/settings`)({
 })
 
 function RouteComponent() {
+  return (
+    <Main>
+      <Section className="animate-fade-down">
+        <Container className="">
+          <Tabs defaultValue="details">
+            <TabsList>
+              <TabsTrigger value="details">Details</TabsTrigger>
+              <TabsTrigger value="api-keys">
+                API Keys <Badge>1</Badge>
+              </TabsTrigger>
+              <TabsTrigger value="team">Team</TabsTrigger>
+              <TabsTrigger value="notifications">Notifications</TabsTrigger>
+            </TabsList>
+            <TabsContent value="details" className="md:max-w-3xl">
+              <DetailsTab />
+            </TabsContent>
+            <TabsContent value="api-keys" className="md:max-w-3xl">
+              <APIKeyTab />
+            </TabsContent>
+            <TabsContent value="team" className="md:max-w-3xl">
+              <TeamTab />
+            </TabsContent>
+            <TabsContent value="notifications" className="md:max-w-3xl">
+              <NotificationTab />
+            </TabsContent>
+          </Tabs>
+        </Container>
+      </Section>
+    </Main>
+  )
+}
+
+const DetailsTab: FC = () => {
   const orgForm = useConsoleForm({
     defaultValues: {
       organizationName: '',
@@ -45,191 +79,182 @@ function RouteComponent() {
     },
   })
 
+  return (
+    <>
+      <Card>
+        <CardHeader>
+          <CardTitle>Organization details</CardTitle>
+          <CardDescription>Manage your organization details</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <form
+            onSubmit={(e) => {
+              e.preventDefault()
+              e.stopPropagation()
+              orgForm.handleSubmit()
+            }}
+            className="space-y-6"
+          >
+            <orgForm.AppField
+              name="organizationName"
+              validators={{
+                onSubmit: ({ value }) => {
+                  if (!value) return 'A valid name is required'
+
+                  return undefined
+                },
+              }}
+            >
+              {(field) => <field.InputField label="Organization Name" />}
+            </orgForm.AppField>
+
+            <orgForm.AppForm>
+              <orgForm.SubmitButton label="Update name" />
+            </orgForm.AppForm>
+          </form>
+        </CardContent>
+      </Card>
+      <Card className="bg-red-500/5 border border-red-500 mt-6">
+        <CardHeader>
+          <CardTitle>Danger zone</CardTitle>
+          <CardDescription>Be careful in this section</CardDescription>
+          <CardContent className="px-0">
+            <List>
+              <ListItem
+                title="Delete your organization"
+                description="Deleting your organization may have unwanted consequences. All data associated with this organization will get deleted and can not be recovered!"
+                prefix={<Trash2 className="text-red-500" />}
+                suffix={
+                  <Button size="lg" variant="destructive">
+                    Delete organization
+                  </Button>
+                }
+              />
+            </List>
+          </CardContent>
+        </CardHeader>
+      </Card>
+    </>
+  )
+}
+
+const APIKeyTab: FC = () => {
   const isMobile = useIsMobile()
 
   return (
-    <Main>
-      <Section className="animate-fade-down">
-        <Container className="">
-          <Tabs defaultValue="details">
-            <TabsList>
-              <TabsTrigger value="details">Details</TabsTrigger>
-              <TabsTrigger value="api-keys">
-                API Keys <Badge>1</Badge>
-              </TabsTrigger>
-              <TabsTrigger value="team">Team</TabsTrigger>
-              <TabsTrigger value="notifications">Notifications</TabsTrigger>
-            </TabsList>
-            <TabsContent value="details" className="md:max-w-3xl">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Organization details</CardTitle>
-                  <CardDescription>
-                    Manage your organization details
-                  </CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <form
-                    onSubmit={(e) => {
-                      e.preventDefault()
-                      e.stopPropagation()
-                      orgForm.handleSubmit()
-                    }}
-                    className="space-y-6"
-                  >
-                    <orgForm.AppField
-                      name="organizationName"
-                      validators={{
-                        onSubmit: ({ value }) => {
-                          if (!value) return 'A valid name is required'
+    <Card>
+      <CardHeader className="flex items-center justify-between">
+        <div>
+          <CardTitle>API Keys</CardTitle>
+          <CardDescription>Manage your API Keys</CardDescription>
+        </div>
+        <Button size={isMobile ? 'icon' : 'sm'}>
+          <span className="hidden lg:inline">Add new key</span>
+          <PlusCircle />
+        </Button>
+      </CardHeader>
+      <CardContent>
+        <Collapsible className="flex max-w-xl flex-col gap-2">
+          <div className="flex items-center justify-between gap-4 px-4">
+            <h4 className="text-sm font-semibold flex items-center">
+              <IconGlobe className="size-4" /> Yoswap
+            </h4>
+            <CollapsibleTrigger asChild>
+              <Button variant="ghost" size="icon" className="size-8">
+                <ChevronsUpDown />
+                <span className="sr-only">Toggle</span>
+              </Button>
+            </CollapsibleTrigger>
+          </div>
+          <CollapsibleContent>
+            <List>
+              <div className="flex flex-col gap-2 px-4">
+                <FatInput
+                  value={'pk_sdvbaosbdvobasd8v9asdviab'}
+                  disabled
+                  contentEditable={false}
+                  suffix={
+                    <Button size="icon" variant="ghost">
+                      <Copy />
+                    </Button>
+                  }
+                />
+                <FatInput
+                  value={'wk_sodasobobasdv98ab'}
+                  disabled
+                  contentEditable={false}
+                  suffix={
+                    <Button size="icon" variant="ghost">
+                      <Copy />
+                    </Button>
+                  }
+                />
+              </div>
+              <ListItem
+                title="Ramp service"
+                description="Enable api key to access the on/off ramp service"
+                suffix={<Switch />}
+              />
+              <ListItem
+                title="Wallet service"
+                description="Enable api key to access the wallet service"
+                suffix={<Switch />}
+              />
+              <ListItem
+                title="Event service"
+                description="Enable api key to access the event service"
+                suffix={<Switch />}
+              />
+            </List>
+          </CollapsibleContent>
+        </Collapsible>
+        <Separator />
+      </CardContent>
+    </Card>
+  )
+}
 
-                          return undefined
-                        },
-                      }}
-                    >
-                      {(field) => (
-                        <field.InputField label="Organization Name" />
-                      )}
-                    </orgForm.AppField>
+const TeamTab: FC = () => {
+  const isMobile = useIsMobile()
 
-                    <orgForm.AppForm>
-                      <orgForm.SubmitButton label="Update name" />
-                    </orgForm.AppForm>
-                  </form>
-                </CardContent>
-              </Card>
-              <Card className="bg-red-500/5 border border-red-500 mt-6">
-                <CardHeader>
-                  <CardTitle>Danger zone</CardTitle>
-                  <CardDescription>Be careful in this section</CardDescription>
-                  <CardContent className="px-0">
-                    <List>
-                      <ListItem
-                        title="Delete your organization"
-                        description="Deleting your organization may have unwanted consequences. All data associated with this organization will get deleted and can not be recovered!"
-                        prefix={<Trash2 className="text-red-500" />}
-                        suffix={
-                          <Button size="lg" variant="destructive">
-                            Delete organization
-                          </Button>
-                        }
-                      />
-                    </List>
-                  </CardContent>
-                </CardHeader>
-              </Card>
-            </TabsContent>
-            <TabsContent value="api-keys" className="md:max-w-3xl">
-              <Card>
-                <CardHeader className="flex items-center justify-between">
-                  <div>
-                    <CardTitle>API Keys</CardTitle>
-                    <CardDescription>Manage your API Keys</CardDescription>
-                  </div>
-                  <Button size={isMobile ? 'icon' : 'sm'}>
-                    <span className="hidden lg:inline">Add new key</span>
-                    <PlusCircle />
-                  </Button>
-                </CardHeader>
-                <CardContent>
-                  <Collapsible className="flex max-w-xl flex-col gap-2">
-                    <div className="flex items-center justify-between gap-4 px-4">
-                      <h4 className="text-sm font-semibold flex items-center">
-                        <IconGlobe className="size-4" /> Yoswap
-                      </h4>
-                      <CollapsibleTrigger asChild>
-                        <Button variant="ghost" size="icon" className="size-8">
-                          <ChevronsUpDown />
-                          <span className="sr-only">Toggle</span>
-                        </Button>
-                      </CollapsibleTrigger>
-                    </div>
-                    <CollapsibleContent>
-                      <List>
-                        <div className="flex flex-col gap-2 px-4">
-                          <FatInput
-                            value={'pk_sdvbaosbdvobasd8v9asdviab'}
-                            disabled
-                            contentEditable={false}
-                            suffix={
-                              <Button size="icon" variant="ghost">
-                                <Copy />
-                              </Button>
-                            }
-                          />
-                          <FatInput
-                            value={'wk_sodasobobasdv98ab'}
-                            disabled
-                            contentEditable={false}
-                            suffix={
-                              <Button size="icon" variant="ghost">
-                                <Copy />
-                              </Button>
-                            }
-                          />
-                        </div>
-                        <ListItem
-                          title="Ramp service"
-                          description="Enable api key to access the on/off ramp service"
-                          suffix={<Switch />}
-                        />
-                        <ListItem
-                          title="Wallet service"
-                          description="Enable api key to access the wallet service"
-                          suffix={<Switch />}
-                        />
-                        <ListItem
-                          title="Event service"
-                          description="Enable api key to access the event service"
-                          suffix={<Switch />}
-                        />
-                      </List>
-                    </CollapsibleContent>
-                  </Collapsible>
-                  <Separator />
-                </CardContent>
-              </Card>
-            </TabsContent>
-            <TabsContent value="team" className="md:max-w-3xl">
-              <Card>
-                <CardHeader className="flex items-center justify-between">
-                  <div>
-                    <CardTitle>Team</CardTitle>
-                    <CardDescription>Manage your Team</CardDescription>
-                  </div>
-                  <Button size={isMobile ? 'icon' : 'sm'}>
-                    <span className="hidden lg:inline">Add team member</span>
-                    <PlusCircle />
-                  </Button>
-                </CardHeader>
-                <CardContent></CardContent>
-              </Card>
-            </TabsContent>
-            <TabsContent value="notifications" className="md:max-w-3xl">
-              <Card>
-                <CardHeader>
-                  <CardTitle>Notifications</CardTitle>
-                  <CardDescription>Manage your Notifications</CardDescription>
-                </CardHeader>
-                <CardContent>
-                  <List>
-                    <ListItem
-                      title="Transactions"
-                      description="Get notified when a transaction occurs"
-                      suffix={<Switch />}
-                    />
-                    <ListItem
-                      title="Events"
-                      description="Get notified when an event is triggered"
-                      suffix={<Switch />}
-                    />
-                  </List>
-                </CardContent>
-              </Card>
-            </TabsContent>
-          </Tabs>
-        </Container>
-      </Section>
-    </Main>
+  return (
+    <Card>
+      <CardHeader className="flex items-center justify-between">
+        <div>
+          <CardTitle>Team</CardTitle>
+          <CardDescription>Manage your Team</CardDescription>
+        </div>
+        <Button size={isMobile ? 'icon' : 'sm'}>
+          <span className="hidden lg:inline">Add team member</span>
+          <PlusCircle />
+        </Button>
+      </CardHeader>
+      <CardContent></CardContent>
+    </Card>
+  )
+}
+
+const NotificationTab: FC = () => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>Notifications</CardTitle>
+        <CardDescription>Manage your Notifications</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <List>
+          <ListItem
+            title="Transactions"
+            description="Get notified when a transaction occurs"
+            suffix={<Switch />}
+          />
+          <ListItem
+            title="Events"
+            description="Get notified when an event is triggered"
+            suffix={<Switch />}
+          />
+        </List>
+      </CardContent>
+    </Card>
   )
 }
