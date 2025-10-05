34fa704fe3a0c1c124dc594d98eea5451514c790
Miracle Friday <miraclef60@gmail.com>
2025-10-05 21:48:36 +0100

    refactor: Update queue and queue.types.ts with event queue for blockchain events

--- packages/shared/Queue/queue.types.ts
+++ packages/shared/Queue/queue.types.ts
@@ -1,3 +1,4 @@
+import { Address } from "viem";
 
 export type RampServiceJob = {
     data: any
@@ -6,3 +7,17 @@ export type RampServiceJob = {
 export type SwapServiceJob = {
     data: any
 };
+
+
+type EventData = {
+  chainId: number;
+  eventName: "Transfer" | "Approval" | string;
+  eventArgType: "from" | "to";
+  address: Address;
+  tokenAddress: Address;
+  persist: boolean;
+  callbackFn?: Function;
+}
+export interface EventServiceJob {
+    data: EventData
+}
--- packages/shared/Queue/queues.ts
+++ packages/shared/Queue/queues.ts
@@ -1,17 +1,19 @@
 import { Queue, QueueOptions } from "bullmq";
 import { CacheFacade } from "Cache/cache.facade";
-import {SwapServiceJob, RampServiceJob} from "./queue.types";
+import {SwapServiceJob, RampServiceJob, EventServiceJob} from "./queue.types";
 
 export const bullMqBase: QueueOptions = { connection: CacheFacade.redisCache };
 
 //NOTE: Make sure the queue name does not have `:` use `-` to avoid redis error
 export enum QueueNames {
   RAMP_QUEUE = "ramp-queue",
-  SWAP_QUEUE = "swap-queue"
+  SWAP_QUEUE = "swap-queue",
+  EVENT_QUEUE = "event-queue"
 }
 
 const rampServiceQueue = new Queue<RampServiceJob, any, "off-ramp" | "on-ramp">(QueueNames.RAMP_QUEUE, bullMqBase);
 const swapServiceQueue = new Queue<SwapServiceJob>(QueueNames.SWAP_QUEUE, bullMqBase);
+const eventServiceQueue = new Queue<EventServiceJob, any, "Transfer" | "Approval">(QueueNames.EVENT_QUEUE, bullMqBase);
 
 // TODO: Implement new queue for payment failed re-retry with different provider
 // const paymentRetryQueue = new Queue<PaymentRetryJob>(QueueNames.PAYMENT_RETRY_QUEUE, bullMqBase);
@@ -22,4 +24,5 @@ const swapServiceQueue = new Queue<SwapServiceJob>(QueueNames.SWAP_QUEUE, bullMq
 export const QueueInstances = {
   [QueueNames.RAMP_QUEUE]: rampServiceQueue,
   [QueueNames.SWAP_QUEUE]: swapServiceQueue,
+  [QueueNames.EVENT_QUEUE]: eventServiceQueue,
 };
