6162b68cb24020cfd5d47d416b438983844437f9
Miracle Friday <miraclef60@gmail.com>
2025-10-05 08:14:19 +0100

    refactor: Hoist banking providers adapter to shared package

--- packages/api/src/lib/adapters/adapters.entities.ts
+++
@@ -1,7 +0,0 @@
-
-export interface PaymentProvider {
-  transfer: (amount: number, recipient: string) => Promise<void>;
-  nameEnquiry: (props: { bankCode: string; accountNumber: string }) => Promise<Record<string, any>>;
-  listBanks: () => Promise<Array<Record<string, any>>>;
-  getTransaction: (id: string) => Promise<Record<string, any>>;
-}
