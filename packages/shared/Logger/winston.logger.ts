a46efcc9accb5ef8b5d518b4759894d1f7e0d64e
Miracle Friday <miraclef60@gmail.com>
2025-10-12 15:37:16 +0100

    feat: Integrate winston logger for global logging to console and better-stack telemetry

---
+++ packages/shared/Logger/winston.logger.ts
@@ -0,0 +1,49 @@
+import winston from "winston"
+import {Logtail} from "@logtail/node"
+import { LogtailTransport} from "@logtail/winston"
+
+const {colorize, combine, timestamp, align, errors} = winston.format
+
+const logtail = new Logtail(process.env.BETTER_STACK_TOKEN_ID!, {
+  endpoint: `https://${process.env.BETTER_STACK_INGESTION_HOST}`
+})
+
+export const logger = winston.createLogger({
+  level: process.env.LOG_LEVEL || "debug",
+  format: process.env.NODE_ENV === "development"? combine(
+    errors({stack: true}),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A' // 2022-01-25 03:23:10.350 PM
+    }),
+    colorize(),
+    align(),
+    // winston.format.json(),
+    winston.format.printf(info => `${info.timestamp} - ${info.level}: ${info.message}`)
+  )
+  : combine(
+    errors({stack: true}),
+    timestamp({
+      format: 'YYYY-MM-DD hh:mm:ss.SSS A' // 2022-01-25 03:23:10.350 PM
+    }),
+    colorize(),
+    align(),
+    winston.format.json(),
+    // winston.format.printf(info => `${info.timestamp} - ${info.level}: ${info.message}`)
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new LogtailTransport(logtail)
+  ],
+  defaultMeta: {
+    service: 'global'
+  },
+  exceptionHandlers: [
+    new winston.transports.Console(),
+    new LogtailTransport(logtail)
+  ],
+  rejectionHandlers: [
+    new winston.transports.Console(),
+    new LogtailTransport(logtail)
+  ],
+  exitOnError: false
+})
