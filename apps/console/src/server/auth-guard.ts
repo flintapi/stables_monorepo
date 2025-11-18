1bef93c8d571f04e6dab75bf8a78ff7cef7e20d9
Miracle Friday <miraclef60@gmail.com>
2025-11-18 13:38:02 +0100

    refactor(console): Deploy 💥

--- apps/console/src/server/auth-guard.ts
+++ apps/console/src/server/auth-guard.ts
@@ -5,6 +5,7 @@ import { redirect } from '@tanstack/react-router'
 
 import { z } from 'zod'
 import { authClient } from '@/lib/auth-client'
+import { env } from '@/env'
 
 export const authGuard = createServerFn()
   .inputValidator(
@@ -16,6 +17,7 @@ export const authGuard = createServerFn()
   )
   .handler(async ({ data }) => {
     const request = getRequest()
+    console.log("Environemnt variable:", env)
     const { data: session } = await authClient.getSession({
       fetchOptions: { headers: request.headers },
     })
