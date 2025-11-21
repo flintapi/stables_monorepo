d8dfe3ae76b7c96901c7cfe3b32283fff1ef6c9f
Miracle Friday <miraclef60@gmail.com>
2025-11-21 21:20:13 +0100

    refactor(synapse): Deploy 💥

--- services/ramp/src/services/ramp.service.ts
+++ services/ramp/src/services/ramp.service.ts
@@ -64,7 +64,7 @@ class Ramp {
     //   }).then((responses) => rampLogger.info("Transfer made and completed...", responses));
     // } else {
       const { transactionId, organizationId, amountReceived, prevProviders } = data;
-      rampLogger.info("Payout transaction with palmpay...", prevProviders, amountReceived);
+      rampLogger.info("Payout transaction with palmpay...", {prevProviders, amountReceived});
       const organization = await db.query.organization.findFirst({
         where(fields, ops) {
           return ops.eq(fields.id, organizationId);
