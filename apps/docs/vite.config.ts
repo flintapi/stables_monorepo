0a734a85745712ceec8533a30eff9a9177b26c06
Miracle Friday <miraclef60@gmail.com>
2025-11-27 01:20:24 +0100

    refactor(docs): Remove unused packages

--- apps/docs/vite.config.ts
+++ apps/docs/vite.config.ts
@@ -6,7 +6,6 @@ import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import mdx from 'fumadocs-mdx/vite';
 import {nitro} from "nitro/vite"
-// import {cloudflare} from "@cloudflare/vite-plugin"
 
 const config = defineConfig({
   plugins: [
@@ -17,7 +16,6 @@ const config = defineConfig({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
-    // cloudflare({ viteEnvironment: { name: 'ssr' } }),
     tanstackStart({
       sitemap: {
         enabled: false
