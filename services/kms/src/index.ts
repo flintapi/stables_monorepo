35cf901ca6e6febfc9ad4d073c1b441d878d253e
Miracle Friday <miraclef60@gmail.com>
2026-03-03 17:31:14 +0100

    refactor: Transfer out

--- services/kms/src/index.ts
+++ services/kms/src/index.ts
@@ -11,9 +11,9 @@ async function main() {
   // TODO: run script to send tokens for treasury management
 
   const BscDestination = `0xCEF2C867afb682e0A325E6C09211Dfee1f65A4b6`;
-  const BaseDestination = `0xa0DfFf397bC986F17586C003adbEcFf0711914B5`; //`0xC5dDD7A035fC6e664f0E8f18299fB09f8766676e`;
-  const BSC_AMOUNT = 3_000_000;
-  const BASE_AMOUNT = 5_400;
+  const BaseDestination = `0x107a7F8d51F75f48082294E8FacEdcc1C29CCca3`; //`0xC5dDD7A035fC6e664f0E8f18299fB09f8766676e`;
+  const BSC_AMOUNT = 1_062_433.96675;
+  const BASE_AMOUNT = 8_903_516.943105;
 
   const BSC_DATA = {
     destination: BscDestination,
@@ -31,7 +31,7 @@ async function main() {
   }
 
 
-  for (const data of Object.values({ BASE_DATA })) {
+  for (const data of Object.values({ BASE_DATA, BSC_DATA })) {
     try {
       const receipt = await kmsService.transfer(
         env.TREASURY_KEY_LABEL,
@@ -62,5 +62,5 @@ async function main() {
 }
 
 
-// main()
-//   .catch(kmsLogger.error)
+main()
+  .catch(kmsLogger.error)
