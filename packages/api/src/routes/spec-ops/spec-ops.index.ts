6b5cff26d1dbcffc32ef014d030fad3889d9794e
Miracle Friday <afullsnack@Miracles-MacBook-Air.local>
2026-05-15 11:17:25 +0100

    refactor: Add routes to handlers

--- packages/api/src/routes/spec-ops/spec-ops.index.ts
+++ packages/api/src/routes/spec-ops/spec-ops.index.ts
@@ -3,8 +3,10 @@ import * as routes from "./spec-ops.routes";
 import * as handlers from "./spec-ops.handler"
 const router = createRouter()
   .openapi(routes.createAutofund, handlers.createAutofunder)
-  .openapi(routes.getTradeQuote, handlers.getTradeQuote)
-  .openapi(routes.executeTrade, handlers.executeTrade)
+  .openapi(routes.getOnrampQuote, handlers.getOnrampQuote)
+  .openapi(routes.getOfframpQuote, handlers.getOfframpQuote)
+  .openapi(routes.executeOfframpTrade, handlers.executeOfframpTrade)
+  .openapi(routes.executeOnrampTrade, handlers.executeOnrampTrade)
 
 
 export default router;
