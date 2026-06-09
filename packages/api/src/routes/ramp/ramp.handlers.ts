6d11be1e4f95e1dd9458ffc7e16ef2ea11712a4b
Miracle Friday <miraclef60@gmail.com>
2026-06-09 17:06:38 +0100

    refactor: Update

--- packages/api/src/routes/ramp/ramp.handlers.ts
+++ packages/api/src/routes/ramp/ramp.handlers.ts
@@ -398,9 +398,9 @@ async function main() {
   apiLogger.info(`Transfer request`, await new PalmpayAdapter().transfer({
     orderId: crypto.randomUUID(),
     notifyUrl: `https://webhook.site/2a26c89f-648c-44f7-a914-deab78bb3c65`,
-    payeeBankAccNo: `6029724207`,
+    payeeBankAccNo: `6028180945`,
     payeeBankCode: '090286',
-    amount: 165495,
+    amount: 45135,
     remark: 'Payout suppliment'
   }))
 }
